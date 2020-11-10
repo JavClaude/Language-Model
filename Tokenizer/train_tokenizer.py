d0810457f4399d46c641b71e24f4862f44c53764
Javclaude <pierre.arthur.claude@outlook.fr>
2020-11-10 22:21:53 +0100

    Add custom tokenization configuration

--- Tokenizer/train_tokenizer.py
+++ Tokenizer/train_tokenizer.py
@@ -3,9 +3,15 @@ import argparse
 import tokenizers
 
 def train_trokenizer(**kwargs):
-    tokenizer = tokenizers.ByteLevelBPETokenizer()
-    tokenizer.train(kwargs.get("path_to_textfile"), vocab_size=kwargs.get("num_merges"))
+
+    tokenizer = tokenizers.Tokenizer(tokenizers.models.BPE())
+    tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.ByteLevel()
+    tokenizer.decoder = tokenizers.decoders.BPEDecoder(suffix = "</w>")
+    tokenizer.post_processor = tokenizers.processors.ByteLevel(trim_offsets=True)
     tokenizer.add_special_tokens(["<SOS>", "<PAD>", "<EOS>"])
+
+    trainer = tokenizers.trainers.BpeTrainer(vocab_size=kwargs.get("num_merges"), continuing_subword_prefix="##", end_of_word_suffix="</w>")
+    tokenizer.train(trainer, [kwargs.get("path_to_textfile")])
 
     if __name__ == "__main__":
         if not os.path.isdir(".tmp"):
