409a8a34c1d3a0578917983682c36c9baf5b5936
Javclaude <pierre.arthur.claude@outlook.fr>
2020-11-10 22:21:11 +0100

    Add Residual connection

--- Model/model.py
+++ Model/model.py
@@ -14,27 +14,39 @@ class LSTMModel(torch.nn.Module):
                 **kwargs):
         super(LSTMModel, self).__init__()
 
+        self.embedding_dim = embedding_dim
         self.n_layers = n_layers
         self.hidden_units = hidden_units
+        self.total_hidden = hidden_units*2 if bidirectional else hidden_units
         self.bidirectional = bidirectional
 
         self.Embedding = torch.nn.Embedding(vocab_size, embedding_dim)
         self.D1 = torch.nn.Dropout(dropout)
         self.LN = torch.nn.LayerNorm(embedding_dim)
         self.Encoder = torch.nn.LSTM(embedding_dim, hidden_units, num_layers=n_layers, batch_first=True, dropout=dropout_rnn, bidirectional=bidirectional)
+        if embedding_dim != self.total_hidden:
+            self.linear_proj = torch.nn.Linear(embedding_dim, self.total_hidden)
+        self.relu = torch.nn.ReLU()
         self.LN2 = torch.nn.LayerNorm(hidden_units)
-        self.D2 = torch.nn.Dropout(dropout)
         self.Decoder = torch.nn.Linear(hidden_units*2 if bidirectional else hidden_units, vocab_size)
-    
-    def forward(self, sequence, hiddens):
-        x = self.Embedding(sequence)
-        x = self.D1(x)
+        self.D2 = torch.nn.Dropout(dropout)
+
+    def forward(self, sequence, hiddens_states):
+        x_embedded = self.Embedding(sequence)
+        x = self.D1(x_embedded)
         x = self.LN(x)
-        x, hiddens = self.Encoder(x, hiddens)
+        hiddens, hiddens_states = self.Encoder(x, hiddens_states)
+        ## Residual connection ##
+        if self.embedding_dim != self.hidden_units:
+            x = torch.add(
+                self.relu(self.linear_proj(x_embedded)), hiddens
+            )
+        else:
+            x = torch.add(self.relu(x_embedded), hiddens)
         x = self.LN2(x)
-        x = self.D2(x)
         logits = self.Decoder(x)
-        return logits, hiddens
+        logits = self.D2(logits)
+        return logits, hiddens_states
     
     def init_hiddens(self, batch_size):
         return (
